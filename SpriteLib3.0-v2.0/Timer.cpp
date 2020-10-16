a3413ad61cb876dd577f04a84d99de046d554167
Joseph Osayande <osazeeenoma.osayande@ontariotechu.net>
2020-10-15 23:03:50 -0400

    Update Timer.cpp

--- SpriteLib3.0-v2.0/Timer.cpp
+++ SpriteLib3.0-v2.0/Timer.cpp
@@ -20,11 +20,15 @@ void Timer::Update()
 {
 	//Moves current clock to the last clock
 	lastClock = currentClock;
+
 	//Creates a new current clock at the current clock time
 	currentClock = clock();
+
 	//Gets the difference betweent he last clock and the current clock
 	//Then divides it by Clocks per second (via multiplying 1.f/CLOCKS_PER_SEC)
+
 	deltaTime = (currentClock - lastClock) * ONE_OVER_CLOCKS_PER_SEC;
+
 	//Adds delta time to the total time passed (since last reset)
 	time += deltaTime;
 }
